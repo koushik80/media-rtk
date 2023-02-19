60ea9a9a549ac99c1c1aaf52586b90a546883153
Koushik Dey <koushik.dey@live.com>
2023-02-19 19:57:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Practice app with react-redux & redux toolkit
+
+</div>
+<h2 align="center"><a href="">Media-RTK</a></h2>
+</div>
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Media app with Redux toolkit
+    </div>
+  )
+}
+
+export default App
+
