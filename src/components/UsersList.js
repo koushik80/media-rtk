022ada721592275f003a062aa3100991a7bb023a
Koushik Dey <koushik.dey@live.com>
2023-02-20 23:56:20 +0200

    refactored user list component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
 import Button from './Button';
 import Skeleton from './Skeleton';
+import UsersListItem from './UsersListItem';
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
@@ -29,13 +30,7 @@ const UsersList = () => {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
-        </div>
-      </div>
-    );
+      return <UsersListItem key={user.id} user={user} />
   });
   }
 
