e11e6616098a1a28d40513c487d6cbf5351da983
Koushik Dey <koushik.dey@live.com>
2023-02-20 15:15:12 +0200

    added skeleton for displaying users list

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,8 +4,19 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3001/users');
 
+  // DEVELOPMENT purpose only
+  await pause(1000);
+
   return response.data;
 });
+
+// DEVELOPMENT purpose only
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
 
