e850a885205f384a3423c193f76c7fcc3b111f92
Koushik Dey <koushik.dey@live.com>
2023-02-22 15:12:01 +0200

    added list of photo items

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,11 @@
 
 
-const PhotosListItem = () => {
-  return 'PhotosListItem';
+const PhotosListItem = ({ photo }) => {
+  return (
+    <div>
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
+    </div>
+  );
 }
 
 export default PhotosListItem;
